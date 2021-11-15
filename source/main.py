4db84ec0702c2ba24b8e12a784e5a9e793610620
ashutosh <jena.ashutosh@gmail.com>
2021-11-15 19:23:39 +0530

    First Commit Ashutosh Branch

--- source/main.py
+++ source/main.py
@@ -1,6 +1,18 @@
 
-print("Hello")
+import logging
+import sys
+import mysqlClient as mc
+import pandas as pd
+import numpy as np
 
-# End of the program
+if __name__ == '__main__':
 
-# Check out Ashutosh Branch
+    logging.basicConfig(stream=sys.stdout, format='%(asctime)s: %(levelname)s - %(message)s',datefmt='%m %d %Y %I:%M:%S %p',level=logging.INFO)
+    logger = logging.getLogger()
+
+    logger.info('In Main Program')
+
+    x = 10
+    list =[1,2,3,4,5]
+    sum = mc.callFunctions(logger,x,list)
+    logger.info("Sum %i ", sum)
